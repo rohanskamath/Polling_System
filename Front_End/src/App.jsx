3f20da5d651be4d460c5eaf72d9d1b5c2b2c4ee0
Rohan Kamath <rohankamath11@gmail.com>
2024-01-05 22:27:47 +0530

    Landing_page Completed

--- Front_End/src/App.jsx
+++ Front_End/src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
 import LandingPage from './Components/LandingPage'
+import PollStation from './Components/PollStation'
+import PollTrends from './Components/PollTrends'
 
 const App = () => {
   return (
     <div>
-      <LandingPage />
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/pollstation' element={<PollStation />} />
+          <Route path='/polltrends' element={<PollTrends />} />
+
+        </Routes>
+
+      </BrowserRouter>
+
     </div>
   )
 }
--- Front_End/src/Components/LandingPage.jsx
+++ Front_End/src/Components/LandingPage.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import PollStation from '../assets/poll_station_img.jpg'
+import PollTrends from '../assets/trend_img.jpg'
+import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
   return (
-    <div>
-        
+    <div className="container-fluid bg-1 text-center py-3">
+      <h3 className="my-4">Polling System</h3>
+
+      <div className="row row-cols-1 row-cols-md-2 g-4 mt-3">
+
+        <Link to="/pollstation" className="col mouse-hover text-decoration-none">
+          <div className="card text-center">
+            <img src={PollStation} className="card-img-top" alt="Poll Station" style={{ height: "280px" }} />
+            <div className="card-body">
+              <h5 className="card-title">Poll Station</h5>
+            </div>
+          </div>
+        </Link>
+
+        <Link to="/polltrends" className="col mouse-hover text-decoration-none">
+          <div className="card">
+            <img src={PollTrends} className="card-img-top" alt="Poll Trends" style={{ height: "280px" }} />
+            <div className="card-body">
+              <h5 className="card-title">Poll Trends</h5>
+            </div>
+          </div>
+        </Link>
+
+      </div>
+
     </div>
   )
 }
